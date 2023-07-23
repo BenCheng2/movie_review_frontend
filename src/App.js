a4cd44f1cc2226e6b21333e1523c23c29af6d528
BenCheng2 <bencheng0609@gmail.com>
2023-07-23 17:50:14 -0400

    docs(Documentation): Cleaning the projects

--- src/App.js
+++ src/App.js
@@ -11,16 +11,12 @@ import NewUserForm from "./features/users/NewUserForm";
 import EditNote from "./features/notes/EditNote";
 import NewNote from "./features/notes/NewNote";
 import Prefetch from "./features/auth/Prefetch";
-import MovieComments from "./features/movieComments/MovieComments";
 import NotesList from "./features/notes/NotesList";
-import NewMovieCommentForm from "./features/movieComments/NewMovieCommentForm";
 import NewMovieComment from "./features/movieComments/NewMovieComment";
-import EditMovieCommentForm from "./features/movieComments/EditMovieCommentForm";
 import EditMovieComment from "./features/movieComments/EditMovieComment";
 import PersistLogin from "./features/auth/PersistLogin";
 import RequireAuth from './features/auth/RequireAuth'
 import {ROLES} from './config/roles'
-import useTitle from "./hooks/useTitle";
 
 
 function App() {
