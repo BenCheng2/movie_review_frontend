6ed567eab03d078f11b842826a3199e61cb0c0bd
BenCheng2 <bencheng0609@gmail.com>
2023-07-23 17:12:55 -0400

    style(Style): Updating Welcome Page

--- src/features/auth/Welcome.js
+++ src/features/auth/Welcome.js
@@ -11,14 +11,14 @@ const Welcome = () => {
         <section className={"welcome"}>
             <p>{today}</p>
             <h1>Welcome {username} !</h1>
-            <p><Link to={"/dash/notes"}>View Movie Notes</Link></p>
-            <p><Link to={"/dash/notes/new"}>Add New Movie Notes</Link></p>
+            <p><Link className="link" to={"/dash/notes"}>View Movie Notes</Link></p>
+            <p><Link className="link" to={"/dash/notes/new"}>Add New Movie Notes</Link></p>
 
-            <p><Link to={"/dash/moviecomments"}>View Movie Comments</Link></p>
-            <p><Link to={"/dash/moviecomments/new"}>Add New Movie Comment</Link></p>
+            <p><Link className="link" to={"/dash/moviecomments"}>View Movie Comments</Link></p>
+            <p><Link className="link" to={"/dash/moviecomments/new"}>Add New Movie Comment</Link></p>
 
-            {(isManager || isAdmin) && <p><Link to={"/dash/users"}>View User Settings</Link></p>}
-            {(isManager || isAdmin) && <p><Link to={"/dash/users/new"}>Add New User</Link></p>}
+            {(isManager || isAdmin) && <p><Link className="link" to={"/dash/users"}>View User Settings</Link></p>}
+            {(isManager || isAdmin) && <p><Link className="link" to={"/dash/users/new"}>Add New User</Link></p>}
         </section>
     )
 
